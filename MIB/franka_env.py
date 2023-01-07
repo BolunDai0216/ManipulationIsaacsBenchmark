6ec47ae3278f3a3972def3290ee15283351f553b
Bolun Dai <bd1555@nyu.edu>
2023-01-07 14:32:46 -0500

    added _franka_env

---
+++ MIB/franka_env.py
@@ -0,0 +1,14 @@
+import math
+import random
+import time
+from pdb import set_trace
+
+import numpy as np
+from isaacgym.torch_utils import *
+from isaacgym import gymapi, gymtorch, gymutil
+import torch
+
+
+class FrankaEnv:
+    def __init__(self):
+        pass
